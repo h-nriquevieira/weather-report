f47c9fe5538a795486ad119e68a346d75f03c1b2
h-nriquevieira <h.nriquevieira@gmail.com>
2022-06-17 11:10:04 -0300

    Working app without styles

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+const dom = {
+    currentTemp: document.querySelector('.current-temp .js-value'),
+    cityName: document.querySelector('.city-name'),
+    tempMax: document.querySelector('.temp-max .js-value'),
+    tempMin: document.querySelector('.temp-min .js-value'),
+    humidity: document.querySelector('.humidity .js-value'),
+    description: document.querySelector('.description'),
+    form: document.querySelector('form'),
+    input: document.querySelector('#city')
+}
+
+async function getData(city) {
+    const resp = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=70101838777dc0e506b142df08a1f81b&units=metric&lang=pt_br`,{mode: 'cors'});
+    const data = await resp.json();
+    return data;
+}
+
+async function formatData(data) {
+    const resp = {
+        name: data.name,
+        temp: data.main.temp,
+        tempMax: data.main.temp_max,
+        tempMin: data.main.temp_min,
+        humidity: data.main.humidity,
+        description: data.weather[0].description
+    }
+    return resp;   
+}
+
+async function startApp(e) {
+    e.preventDefault();
+    const data = await getData(dom.input.value);
+    const formated = await formatData(data);
+    dom.currentTemp.textContent = await formated.temp;
+    dom.cityName.textContent = await formated.name;
+    dom.tempMax.textContent = await formated.tempMax;
+    dom.tempMin.textContent = await formated.tempMin;
+    dom.humidity.textContent = await formated.humidity;
+    dom.description.textContent = await formated.description;
+}
+
+dom.form.addEventListener('submit', startApp);
